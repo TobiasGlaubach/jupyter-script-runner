786fd2e83cb06de471a72d68ea527a1b1a827832
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2024-08-27 08:10:14 +0200

    added get_sys_info to helpers

--- src/JupyRunner/core/helpers.py
+++ src/JupyRunner/core/helpers.py
@@ -98,3 +98,50 @@ def split_flat_dict_into_nested(flat_dict):
       current_dict = current_dict[k]
     current_dict[keys[-1]] = value
   return nested_dict
+
+
+def get_sys_info():
+    """
+    Gathers and formats system information into a string.
+
+    Returns:
+        A string containing formatted system information.
+    """
+
+    info_string = ''
+    info_string += f"{'=' * 40} System Information {'=' * 40}\n\n"
+
+    try:
+        import platform
+        
+        import getpass
+        import datetime
+
+        username = getpass.getuser()
+        
+        uname = platform.uname()
+
+        # Build the system information string
+        
+        info_string += f"User:           {username}\n"
+        info_string += f"System:         {uname.system}\n"
+        info_string += f"Node Name:      {uname.node}\n"
+        info_string += f"Release:        {uname.release}\n"
+        info_string += f"Version:        {uname.version}\n"
+        info_string += f"Machine:        {uname.machine}\n"
+        info_string += f"Processor:      {uname.processor}\n"
+
+        from psutil import virtual_memory
+        mem = virtual_memory()
+        info_string += f"RAM:            {mem.total / (1024 * 1024 * 1024):.1f} GB\n\n"
+
+
+    except Exception as e:
+        # Handle exceptions gracefully (e.g., log the error)
+        info_string += f"\nERROR while trying to print system info: {e}"
+    
+    info_string += f"\nCurrent date and time : {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
+    info_string += f"                  ISO : {datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()}\n"
+    info_string += f"\n{'=' * 40} System Information {'=' * 40}"
+    
+    return info_string
