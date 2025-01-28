8d7fac7b80f858ede561cbad9b22fe18d8586147
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2025-01-28 17:06:51 +0100

    added testcase (the testcase decorator)

---
+++ src/JupyRunner/client/testcase.py
@@ -0,0 +1,156 @@
+
+
+# helper function instead of assert
+def asserte(err, to_test, message, do_print=True):
+    if not to_test:
+        if do_print:
+            print_color('Testcondition failed. Message: ' + message, 'red')
+        return err + 'ERROR: ' + message + '\n'
+    else:
+        return err
+    
+import functools
+
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    BLACK = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+colors_dc = {
+    'header': colors.HEADER,
+    'blue': colors.OKBLUE,
+    'green': colors.OKGREEN,
+    'warn': colors.WARNING,
+    'red': colors.FAIL,
+    'black': colors.BLACK,
+    'bold': colors.BOLD,
+    'underline': colors.UNDERLINE
+}
+
+_print = print
+
+def print_color(msg, color='red'):
+    if isinstance(color, str):
+        color = colors_dc[color]
+    if print.do_log:
+        print.log.append(msg)
+    _print(f"{color}{msg}{colors.BLACK}")
+
+def print(*args, **kwargs):
+    if print.do_log:
+        print.log.append(''.join([str(arg) for arg in args]))
+    _print(*args, **kwargs)
+
+print.log = []
+print.do_log = False
+
+def testcase(_func=None, *, test_name='', func_to_get_chan_values=None, expected_chan_values=None, n_repeat=1):
+
+    def decorator_name(func):
+        @functools.wraps(func)
+        def wrapper():
+            global results
+
+            for i in range(n_repeat):
+                print.log.clear()
+                print.do_log = True
+            
+                fname = 'TESTCASE No. {: 4d} | "{}"'.format(len(results), func.__name__)
+                if test_name:
+                    fname += f' | "{test_name}"'
+                    
+                name = func.__name__ if not test_name else test_name
+
+                print_color((('-'*20) + ' RUNNING ' + fname + '-'*20).ljust(100, '-'), 'bold')
+                print_color(fname + 'running...', 'blue')
+
+                t_start = datetime.datetime.utcnow()
+                time.sleep(0.1)
+                
+
+                try:
+                    
+                    # RUN the Testcase
+                    
+                    err = func()
+
+                    assert not err is None, 'every testcase need to return an error string. Please return "" (empty string) if you have no error '
+                    if err is None:
+                        err = ''
+                    
+                    
+                except Exception as err_obj:   
+                    print_color(fname + '--> FAIL', 'red')
+                    s = 'ERROR while executing:\n' + traceback.format_exc()
+                    print_color(s, 'red')
+                    # ADD TO RESULTS
+                    results.append((name, s))
+                    return 
+                
+                if err:
+                    print_color(fname + '--> FAIL', 'red')
+                else:
+                    print_color(fname + '--> OK', 'green')
+
+                time.sleep(0.1)
+                t_end = datetime.datetime.utcnow()
+
+                # ---------------
+                # TEST ERROR CHANNELS
+                # ---------------
+                if expected_chan_values and func_to_get_chan_values:
+                    print_color(fname + 'testing for channel errors...', 'blue')
+
+                    chans = func_to_get_chan_values(expected_chan_values.keys(), t_start, t_end)
+                    err_chans = [c for c in expected_chan_values if expected_chan_values[c](chans[c]) ]
+
+                    if err_chans:
+                        err += 'Error on channel(s): ' + ', '.join(err_chans) + '\n'
+                        print_color(fname + '--> FAIL', 'red')
+                    else:
+                        print_color(fname + '--> OK', 'green')
+
+                
+                print_color((('-'*10) + ' DONE ' + ('-'*10)).ljust(100, '-'), 'header')
+                if err:
+                    s = fname + '--> FAILED!'
+                    s += '\n' + err
+
+                    print_color(s, 'red')
+                    print_color('-'*100, 'red')
+                else:
+                    s = fname + '--> SUCCESS!'
+                    print_color(s, 'green')
+                    print_color('-'*100, 'green')
+
+                print.do_log = False
+                txt = '\n'.join(print.log)
+                    
+                    
+                if add_test_info_to_rep:
+                    doc.add_kw('markdown', '#### ' + fname + '\n this is the individual test output while running the test:\n\n', chapter='Testcases')
+                    
+                    for c in colors_dc.values():
+                        txt = txt.replace(c, '')
+                    doc.add_kw('verbatim', txt, chapter='Testcases')
+                    # doc.add_kw('verbatim', '\n'.join(print_color.log), chapter='Testcases')
+
+
+                # ADD TO RESULTS
+                results.append((name, err))
+                
+
+        return wrapper
+    
+    if _func is None:
+        return decorator_name                      # 2
+    else:
+        return decorator_name(_func)               # 3
+    
+    
