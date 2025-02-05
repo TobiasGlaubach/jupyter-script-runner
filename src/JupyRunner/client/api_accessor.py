0c147ec24efa03f96c63abcdec64ab5bbad71618
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2025-02-05 10:40:16 +0100

    reworked the api_accessor to allow for better user interaction

--- src/JupyRunner/client/api_accessor.py
+++ src/JupyRunner/client/api_accessor.py
@@ -1,8 +1,9 @@
 
+import hashlib
 import io
 import mimetypes
 import time
-import requests, datetime, enum, re, dateutil, logging, sys
+import requests, datetime, enum, re, dateutil, sys
 
 from typing import Any, Dict, List, Union
 
@@ -16,8 +17,11 @@ if __name__ == '__main__':
     sys.path.insert(0, parent_dir)
     
 
-from JupyRunner.core.helpers import log, make_zulustr, parse_zulutime, iso_now, get_primary_ip, get_sys_id
+from JupyRunner.core.helpers import limit_len, logging, log, make_zulustr, parse_zulutime, iso_now, get_primary_ip, get_sys_id, get_uid
     
+log_parent = log
+log = log_parent.getChild('jpy_client')
+log.setLevel(logging.INFO)
 
 
 class BaseAPIClient:
@@ -85,22 +89,24 @@ class BaseAPIClient:
         
     def put(self, endpoint, json=None, headers=None, ret_raw=False):
         url = self._url(endpoint)
-        data = self._json(data)
-        log.debug(f'PUT: {url} {data=}')
+        json = self._json(json)
+        s = f'{json=}'
+        log.debug(f'PUT: {url} {limit_len(s, 200)}')
         response = self.session.put(url, json=json, headers=headers)
         response.raise_for_status()  # Raise an exception for error responses
 
         if ret_raw: 
             return response
         resp = response.json()
-        self.validate(data, resp)
+        self.validate(json, resp)
         return resp
 
 
     def post(self, endpoint, json=None, headers=None, files=None, ret_raw=False):
         url = self._url(endpoint)
         json = self._json(json)
-        log.debug(f'POST: {url} {json=}')
+        s = f'{json=}'
+        log.debug(f'POST: {url} {limit_len(s, 200)}')
         response = self.session.post(url, json=json, headers=headers, files=files)
         response.raise_for_status()  # Raise an exception for error responses
         if ret_raw: 
@@ -112,8 +118,8 @@ class BaseAPIClient:
 
     def patch(self, endpoint, json:Dict[str, Any]|None=None, headers=None, ret_raw=False):
         url = self._url(endpoint)
-        json = self._json(json)
-        log.debug(f'POST: {url} {json=}')
+        s = f'{json=}'
+        log.debug(f'PATCH: {url} {limit_len(s, 200)}')
         response = requests.patch(url, json=json, headers=headers)
         response.raise_for_status()  # Raise an exception for error responses
         if ret_raw: 
@@ -125,7 +131,8 @@ class BaseAPIClient:
 
 
 class ServerApi(object):
-    def __init__(self, base_url, none_on_404=False) -> None:
+    def __init__(self, base_url, none_on_404=False, script_id=None) -> None:
+        self.script_id = script_id
         self.api = BaseAPIClient(base_url, none_on_404)
 
     def ping(self):
@@ -136,17 +143,22 @@ class ServerApi(object):
         assert device_id, 'device_id can not be None or empty!'
         return self.api.get(f'device/{device_id}')
     
-    def get_script(self, script_id):
+    def get_script(self, script_id=None):
+        if script_id is None:
+            script_id = self.script_id
         assert script_id, 'script_id can not be None or empty!'
         return self.api.get(f'script/{script_id}')
 
 
-    def get_script_full(self, script_id):
+    def get_script_full(self, script_id=None):
+        if script_id is None:
+            script_id = self.script_id
+
         assert script_id, 'script_id can not be None or empty!'
         return self.api.get(f'script_full/{script_id}')
 
 
-    def upload_file(self, script_id, filename:str, byte_data:bytes, mimetype=None):
+    def upload_file(self, script_id=None, filename:str='', byte_data:bytes=b'', mimetype=None):
         """Uploads a file to a script.
 
         This method uploads a file to a script using the provided byte data.
@@ -166,12 +178,18 @@ class ServerApi(object):
         Raises:
             TypeError: If `byte_data` is not a bytes object or `filename` is not a string.
         """
+        if script_id is None:
+            script_id = self.script_id
+        assert script_id, 'script_id can not be None or empty!'
 
         route = f'/action/script/{script_id}/upload/files'
 
         if isinstance(byte_data, str):
             byte_data = byte_data.encode()
         
+        assert byte_data and isinstance(byte_data, bytes), f'need to give non empty bytes object as byte_data but got {type(byte_data)=} with {byte_data=}'
+        assert filename and isinstance(filename, str), f'need to give non empty str object as filename but got {type(filename)=} with {filename=}'
+
         if not mimetype:
             mime, _ = mimetypes.guess_type(filename.split(".")[-1])
             mimetype = 'application/octet-stream' if mime is None else mime
@@ -182,7 +200,7 @@ class ServerApi(object):
         return self.api.post(route, files=files, ret_raw=True).json()
             
 
-    def upload_files(self, script_id, files:Dict[str, Union[str, bytes]]):
+    def upload_files(self, script_id=None, files:Dict[str, Union[str, bytes]]=None):
         """Uploads multiple files to a script.
 
         This method uploads a dictionary of files to a script. Keys in the dictionary
@@ -203,8 +221,14 @@ class ServerApi(object):
             ValueError: If any value in the `files` dictionary is not a bytes object
                 or a string.
         """
+        if script_id is None:
+            script_id = self.script_id
+        assert script_id, 'script_id can not be None or empty!'
 
         route = f'/action/script/{script_id}/upload/files'
+
+        assert files and isinstance(files, dict), f'need to give non empty str object as filename but got {type(filename)=} with {filename=}'
+
 
         tuples = []
         if isinstance(files, dict):
@@ -218,14 +242,16 @@ class ServerApi(object):
             mime, _ = mimetypes.guess_type(filename.split(".")[-1])
             mimetype = 'application/octet-stream' if mime is None else mime
                 
-            assert isinstance(byte_data, bytes), 'can only upload bytes or strings to the server!'
+            assert byte_data and isinstance(byte_data, bytes), f'need to give non empty bytes object as byte_data but got {type(byte_data)=} with {byte_data=}'
+            assert filename and isinstance(filename, str), f'need to give non empty str object as filename but got {type(filename)=} with {filename=}'
+
             tuples.append((filename, io.BytesIO(byte_data), mimetype))
 
         return self.api.post(route, files={'files': tuples}, ret_raw=True).json()
             
 
 
-    def upload_doc(self, doc, doc_name='', force_overwrite=False, page_title='', script_id=''):
+    def upload_doc(self, doc, doc_name='', force_overwrite=False, page_title='', script_id=None, validate=True, crop_result=True):
         """Uploads the document data to the server
             The json body is constructed as:
             
@@ -237,11 +263,13 @@ class ServerApi(object):
             }
 
         Args:
-            url (str): The URL of the endpoint that accepts the document data.
+            doc (pydocmaker.Doc): The document to upload, either a pydocmaker.Doc object, or a list.
             doc_name (str, optional): The name of the uploaded document. Defaults to ''.
             force_overwrite (bool, optional): Whether to overwrite an existing document. Defaults to False.
             page_title (str, optional): The title of the uploaded document (if applicable). Defaults to ''.
             requests_kwargs: (dict, optional) with kwargs for requests.post(). Defaults to None.
+            validate (bool, optional): Whether or not to validate the result for success and raise an assertion error if not successful. Defaults to True.
+            validate (bool, optional): Whether or not to crop the script from the returned results to be more concide. Defaults to True.
 
         Returns:
             dict: The JSON response from the server after uploading the document.
@@ -250,18 +278,94 @@ class ServerApi(object):
             requests.exceptions.RequestException: If the upload request fails.
         """
 
+        if script_id is None:
+            script_id = self.script_id
+
         route = f'/action/script/{script_id}/upload/doc' if script_id else '/doc/upload'
+        
+
+        if hasattr(doc, 'dump'): # if its a 
+            doc = doc.dump()
+
+        assert isinstance(doc_name, str), f'doc_name needs to be a string but was {type(doc_name)=} {doc_name=}'
+        assert isinstance(page_title, str), f'page_title needs to be a string but was {type(page_title)=} {page_title=}'
+        assert isinstance(force_overwrite, (bool, int)), f'force_overwrite needs to be a bool or int but was {type(force_overwrite)=} {force_overwrite=}'
+        assert isinstance(doc, list), f'doc needs to be a list (or needs to be able to do "doc.dump() -> list") but was {type(doc)=} {doc=}'
+
+        # invalid_types = [k for k in doc if not isinstance(k, (dict, str, list))]
         
         upload = {
             "doc_name": doc_name,
-            "doc": doc.dump(),
+            "doc": doc,
             "force_overwrite": force_overwrite,
             "page_title": page_title
         }
+        result = self.api.post(route, json=upload, ret_raw=True).json()
 
-        return self.api.post(route, json=upload, ret_raw=True).json()
+        if validate:
+            assert result.get('ok'), f'result object did not indicate success by ok=True {result=}'
 
-    def get_user_feedback(self, msg, request_type='okcancle', script_id=None, request_id=None, t_poll_sec=2.0, verb=0, ret_all=False):
+        if crop_result:
+            result.pop('script', None)
+
+        return result
+
+    def user_info(self, msg, color='', script_id=None, request_id=None, script_name='', doc=None):
+        """Logs a message for the user to the server. (This will show up in the fser feedback screen)
+
+        Args:
+            msg (str or pydocmaker.Doc): The message to log. If a pydocmaker.Doc is provided, the doc will be converted to HTML.
+            color (str, optional): The color to use for the message. Defaults to ''.
+            script_id (str, optional): The ID of the script. If not provided, defaults to self.script_id.
+            request_id (str, optional): The ID of the request. If not provided, a new ID will be generated.
+            script_name (str, optional): The name of the script. Defaults to ''.
+            doc (Document, optional): A pydocmaker.Doc document to add to the post. If provided, the doc will be converted to HTML.
+
+        Returns:
+            dict: The JSON response from the API.
+        """
+
+        request_type = 'info'
+        if script_id is None:
+            script_id = self.script_id
+
+        if not request_id:
+            request_id = get_uid(self, script_id)
+
+        
+
+        html = ''
+        if not isinstance(msg, str) and hasattr(msg, 'to_html'):
+            assert not doc, 'can not give doc and a doc as message!'
+            html = msg.to_html()
+            msg = 'HTML Document below...'
+        if doc:
+            html = doc.to_html()
+        
+        
+        route = f'/user_feedback/create'
+        data = {
+            'message': msg,
+            'request_type': request_type,
+            'id': request_id,
+            'script_id': script_id,
+            'script_name': script_name
+        }
+
+        if color or html:
+            data["kwargs"] = {}
+            
+        if color:
+            data["kwargs"]["color"] = color
+        if html:
+            data["kwargs"]["html"] = html
+
+        log.info(f'Sending User Info with {request_id=} {request_type=} and {script_id=}. Message = {limit_len(msg, 200)}')
+        res = self.api.post(route, json=data, ret_raw=True).json()
+        return res
+    
+
+    def user_get_feedback(self, msg, request_type='confirm', script_id=None, request_id=None, t_poll_sec=2.0, verb=0, ret_all=False, doc=None):
         """
         Gets user feedback through the API.
 
@@ -271,11 +375,11 @@ class ServerApi(object):
         check for user response.
 
         Args:
-            msg (str): The message to show the user for feedback.
+            msg (str or pydocmaker.Doc): The message to show the user for feedback. If a pydocmaker.Doc is provided, the doc will be converted to HTML.
             request_type (str, optional): The type of feedback request. Must be one of 
-                'okcancle', 'file', 'files', 'picture', 'pictures', 'text', or 'number'. 
-                Defaults to 'okcancle'.
-            script_id (str, optional): The ID of the script requesting the feedback.
+                'confirm', 'file', 'files', 'picture', 'pictures', 'text', or 'number'. 
+                Defaults to 'confirm'.
+            script_id (str, optional): The ID of the script requesting the feedback (if not given the objects given script_id will be used). 
             request_id (str, optional): A unique identifier for the feedback request. 
                 If not provided, a new one will be generated.
             t_poll_sec (float, optional): The time (in seconds) to wait between polling 
@@ -285,21 +389,35 @@ class ServerApi(object):
                 (no logging).
             ret_all (bool, optional): set to True or 1 to return the raw response dict
                 instead of a casted type.
+            doc (Document, optional): A pydocmaker.Doc document to add to the post. If provided, the doc will be converted to HTML.
 
         Returns:
-            dict: The user feedback response as a dictionary, or None if the user has 
-                not responded within the polling loop.
+            Union[bool, str, int, float, dict]: The user feedback response, which can be a boolean,
+            string, integer, float, or dictionary, depending on the request type. If `ret_all` is True,
+            the raw response dictionary is returned instead.
 
         Raises:
             AssertionError: If the provided `request_type` is not a valid option.
         """
 
+        if script_id is None:
+            script_id = self.script_id
+
         if not request_id:
-            request_id = f'{iso_now()}_{id(self)}_{get_sys_id()}_{get_primary_ip()}'
+            request_id = get_uid(self, script_id)
+
         allowed = 'confirm file files picture pictures text int float'.split()
 
         assert request_type in allowed, f'{request_type=} is not in {allowed=}'
 
+        html = ''
+        if not isinstance(msg, str) and hasattr(msg, 'to_html'):
+            assert not doc, 'can not give doc and a doc as message!'
+            html = msg.to_html()
+
+        if doc:
+            html = doc.to_html()
+        
         route = f'/user_feedback/create'
         data = {
             'message': msg,
@@ -308,6 +426,10 @@ class ServerApi(object):
             'script_id': script_id,
         }
 
+        if html:
+            data["kwargs"] = {"html": html}
+
+        log.info(f'Requesting User Feedback with {request_id=} {request_type=} and {script_id=}. Message = {limit_len(msg, 200)}')
         res = self.api.post(route, json=data, ret_raw=True).json()
 
 
