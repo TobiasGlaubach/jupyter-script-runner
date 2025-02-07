ee54c94db62ef7fe871add7aceecae9f30e9b91e
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2025-02-07 20:50:22 +0100

    implemented user_info_verbosity

--- src/procserver.py
+++ src/procserver.py
@@ -51,6 +51,7 @@ api = runner.api
 api_log = capi.ServerApi(config.get('globals', {}).get('dbserver_uri'))
 
 run_directly = config.get('procserver', {}).get('do_direct_running', 0)
+user_info_verbosity = config.get('procserver', {}).get('user_info_verbosity', 0)
 
 commit = runner.commit
 set_prop_remote = runner.set_prop_remote
@@ -128,14 +129,17 @@ def finish(p, id):
         filename_without_extension = os.path.splitext(os.path.basename(obj.script_out_path))[0]
 
         try:
-            api_log.user_info('Procserver: ' + s, color='red', script_id=id, script_uid=filename_without_extension, device_id=obj.device_id)
+            if user_info_verbosity > 0: 
+                api_log.user_info('Procserver: ' + s, color='red', script_id=id, script_uid=filename_without_extension, device_id=obj.device_id)
+            
         except Exception as err:
             pass
         
 
     else:
         try:
-            api_log.user_info('Procserver: FINISHED', color='grey', script_id=id, script_uid=filename_without_extension, device_id=obj.device_id)
+            if user_info_verbosity > 0:
+                api_log.user_info('Procserver: FINISHED', color='grey', script_id=id, script_uid=filename_without_extension, device_id=obj.device_id)
         except Exception as err:
             pass
 
